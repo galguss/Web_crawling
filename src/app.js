a09f41c219d3b01cdc1b42640632eda222b6e502
galguss <gal.gus12@gmail.com>
2023-01-08 13:47:34 +0200

    Building the infrastructure of the application

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const app = express();
+
+const post = process.env.POST || 3000;
+
+dotenv.config();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+const publicPath = path.join(__dirname, '../public');
+app.use(express.static(publicPath));
+
+app.set('view engine','ejs');
+
+const htmlFile = path.join(__dirname, "../views");
+app.set('views', htmlFile);
+
+app.listen(post, (req, res) => console.log("server is running..."));
+
+app.get('/', (req, res) => {
+    res.render('index');
+})
